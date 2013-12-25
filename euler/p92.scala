8f053c9cb8ce8aea93f134de2c64cdf6676280eb
Boshen Chen <boshenc@gmail.com>
2013-12-25 23:12:09 +1300

    [euler] improve 92

--- euler/p92.scala
+++ euler/p92.scala
@@ -1,3 +1,3 @@
-def foo(n: Long): Stream[Long] =
-  Stream.cons(n, foo(n.toString.map(_.asDigit).map(x=>x*x).sum))
-println( (1 to 10000000).view.map(x=>foo(x.toLong).find(x=>x==89 || x==1).get).filter(_==89).size )
+def foo(n: Int): Stream[Int] =
+  n #:: foo(n.toString.map(_.asDigit).map(x=>x*x).sum)
+println((1 to 10000000).view.map(n=>foo(n).dropWhile(x=>x!=89 && x!=1).head).count(_==89))
